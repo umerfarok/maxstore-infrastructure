2f4651f5d63b1465f689fdbb1fd9135a7d942878
umerfarok <umerfarooq.dev@gmail.com>
2024-04-07 12:14:39 +0500

    Add S3 bucket ownership controls and public access block

--- modules/s3/main.tf
+++ modules/s3/main.tf
@@ -1,12 +1,37 @@
 resource "aws_s3_bucket" "bucket" {
   bucket = var.bucket_name
+
   tags = {
     Name        = "maxstore"
     Environment = "Dev"
   }
 }
 
+resource "aws_s3_bucket_ownership_controls" "bucket_ownership_controls" {
+  bucket = aws_s3_bucket.bucket.id
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
+}
 
+resource "aws_s3_bucket_public_access_block" "bucket_pab" {
+  bucket = aws_s3_bucket.bucket.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
+resource "aws_s3_bucket_acl" "bucket_acl" {
+  depends_on = [
+    aws_s3_bucket_ownership_controls.bucket_ownership_controls,
+    aws_s3_bucket_public_access_block.bucket_pab,
+  ]
+
+  bucket = aws_s3_bucket.bucket.id
+  acl    = "public-read"
+}
 resource "aws_s3_bucket_cors_configuration" "bucker_cors" {
   bucket = aws_s3_bucket.bucket.id
 
